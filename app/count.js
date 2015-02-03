e1cc8cc19f3ecf56cfb94f04ddab6295f8d4b7c4
Casey White <casey.r.white@gmail.com>
2015-02-02 16:16:31 -0800

    complete counter test

--- app/count.js
+++ app/count.js
@@ -3,6 +3,24 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      var timeout;
+      var current = start;
+
+      var count = function() {
+        if (current <= end) {
+          console.log(current);
+          current++;
+          timeout = setTimeout(count, 100);
+        }
+      };
+
+      count();
+
+      return {
+        cancel: function() {
+          if (timeout) clearTimeout(timeout);
+        }
+      };
     }
   };
 });
